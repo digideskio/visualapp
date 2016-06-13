23b89bae00ad2cebe79cdf4f913389a19523dbc0
Vladimir Barbarosh <vladimir.barbarosh@gmail.com>
2016-06-13 13:20:45 +0300

    introduce EDITOR_ env vars

--- src/route/page.jsx
+++ src/route/page.jsx
@@ -3,11 +3,11 @@ import express from 'express';
 var router = express.Router();
 
 router.get('/view', function (req, res) {
-    res.send('view');
+    res.redirect(process.env.EDITOR_VIEW);
 });
 
 router.get('/edit', function (req, res) {
-    res.end('edit');
+    res.redirect(process.env.EDITOR_EDIT);
 });
 
 export default router;
